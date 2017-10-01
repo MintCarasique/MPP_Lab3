71ef52723869fbaa618a0c071a4cd86fb8b35ad0
Artem Slizhevskiy <mykew98@gmail.com>
2017-10-01 12:42:08 +0300

    Initial commit

---
+++ Lab3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int AmountOfTasks = 10;
+            ManualResetEvent[] doneEvents = new ManualResetEvent[AmountOfTasks];
+            Console.WriteLine("Enter source folder path:");
+            string SourcePath = Console.ReadLine();
+            Console.WriteLine("Enter destination folder path:");
+            string DestPath = Console.ReadLine();
+            Copier FileCopier = new Copier();
+            
+        }
+    }
+}
